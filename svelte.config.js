686c6c9ccc8c6d92f9f9c847fb5885bdb769c3ed
VaughnGit <alvin@vaughns.net>
2023-04-08 12:49:24 -0500

    fix: svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,25 @@
  import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
-// export default {
-//   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-//   // for more information about preprocessors
-//   preprocess: vitePreprocess()
-// }
-
-import adapter from '@sveltejs/adapter-static'
-
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-  preprocess: vitePreprocess(),
-  kit: {
-    adapter: adapter(),
-    prerender: {
-      default: true
-    }
-  }
+export default {
+  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
+  // for more information about preprocessors
+  preprocess: vitePreprocess()
 }
 
-export default config
+
+// import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+
+// import adapter from '@sveltejs/adapter-static'
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+//   preprocess: vitePreprocess(),
+//   kit: {
+//     adapter: adapter(),
+//     prerender: {
+//       default: true
+//     }
+//   }
+// }
+
+// export default config
