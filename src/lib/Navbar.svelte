483cf4ffd9e390a1c199db8b45758e8887e3ccee
VaughnGit <alvin@vaughns.net>
2023-04-10 09:58:37 -0500

    doc: convert to chatbot verbaige

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -6,7 +6,7 @@
   <div class="navbar-brand">
     <a class="navbar-item" href={'#/'}>
       <img src={logo} alt="ChatGPT-web" width="28" height="28" />
-      <p class="ml-2 is-size-4 has-text-weight-bold">ChatGPT-web</p>
+      <p class="ml-2 is-size-4 has-text-weight-bold">ChatBot-web</p>
     </a>
   </div>
 </nav>
